7cda1ad68cc4d0515cf66bc3cc105155c0127cb8
Soutaro Matsumoto <matsumoto@soutaro.com>
2019-03-10 17:24:41 +0900

    bundle gem

---
+++ lib/ruby/signature.rb
@@ -0,0 +1,8 @@
+require "ruby/signature/version"
+
+module Ruby
+  module Signature
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ test/ruby/signature_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class Ruby::SignatureTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Ruby::Signature::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
