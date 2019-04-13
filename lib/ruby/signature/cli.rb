4873fa88e163085c8493cffa2359d8af5b3cce12
Soutaro Matsumoto <matsumoto@soutaro.com>
2019-04-14 01:54:22 +0900

    Fix ast command

--- lib/ruby/signature/cli.rb
+++ lib/ruby/signature/cli.rb
@@ -3,6 +3,32 @@ require "optparse"
 module Ruby
   module Signature
     class CLI
+      class LibraryOptions
+        attr_reader :libs
+        attr_reader :dirs
+        attr_accessor :no_stdlib
+
+        def initialize()
+          @libs = []
+          @dirs = []
+          @no_stdlib = false
+        end
+
+        def setup(loader)
+          libs.each do |lib|
+            loader.add(library: lib)
+          end
+
+          dirs.each do |dir|
+            loader.add(path: Pathname(dir))
+          end
+
+          loader.stdlib_root = nil if no_stdlib
+
+          loader
+        end
+      end
+
       attr_reader :stdout
       attr_reader :stderr
 
@@ -13,11 +39,33 @@ module Ruby
 
       COMMANDS = [:ast, :version]
 
+      def library_parse(opts, options:)
+        opts.on("-r LIBRARY") do |lib|
+          options.libs << lib
+        end
+
+        opts.on("-I DIR") do |dir|
+          options.dirs << dir
+        end
+
+        opts.on("--no-stdlib") do
+          options.no_stdlib = true
+        end
+
+        opts
+      end
+
       def run(args)
+        options = LibraryOptions.new
+
+        OptionParser.new do |opts|
+          library_parse(opts, options: options)
+        end.parse!(args)
+
         command = args.shift&.to_sym
 
         if COMMANDS.include?(command)
-          __send__ :"run_#{command}", args
+          __send__ :"run_#{command}", args, options
         else
           run_help()
         end
@@ -27,23 +75,11 @@ module Ruby
         stdout.puts "Available commands: #{COMMANDS.join(", ")}"
       end
 
-      def run_ast(args)
+      def run_ast(args, options)
         env = Environment.new()
         loader = EnvironmentLoader.new(env: env)
 
-        OptionParser.new do |opts|
-          opts.on("-r LIBRARY") do |lib|
-            loader.add(library: lib)
-          end
-
-          opts.on("--no-stdlib") do
-            loader.stdlib_root = nil
-          end
-        end.parse!
-
-        args.each do |path|
-          loader.add(path: Pathname(path))
-        end
+        options.setup(loader)
 
         loader.load
 
