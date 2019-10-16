87a9821a668b06568b4062dc8246c48f0dc03a95
Seiei Miyagi <hanachin@gmail.com>
2019-10-16 22:04:18 +0900

    Fix warning: assigned but unused variable

--- test/ruby/signature/test_test.rb
+++ test/ruby/signature/test_test.rb
@@ -91,7 +91,7 @@ EOF
                          types: ["() { (::String) -> void } -> ::String"])
 
         hook.run do
-          foo = klass.open {|foo|
+          _foo = klass.open {|foo|
             1 + 2 + 3
           }
         end
@@ -132,7 +132,7 @@ EOF
         hook = Ruby::Signature::Test::Hook.install(env, klass, logger: logger).verify_all
 
         hook.run do
-          foo = klass.open {
+          _foo = klass.open {
             1 + 2 + 3
 
             self.foo(1, 2, 3)
