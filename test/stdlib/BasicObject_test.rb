7e521973f448ac96758728ea9c670705fbaf253a
Soutaro Matsumoto <matsumoto@soutaro.com>
2019-10-27 23:43:19 +0900

    Fix BasicObject"instance_exec

--- test/stdlib/BasicObject_test.rb
+++ test/stdlib/BasicObject_test.rb
@@ -5,4 +5,9 @@ class BasicObjectTest < StdlibTest
   def test_instance_eval
     BasicObject.new.instance_eval { |x| x }
   end
+
+  def test_instance_exec
+    BasicObject.new.instance_exec(1) { 10 }
+    BasicObject.new.instance_exec(1,2,3) { 10 }
+  end
 end
