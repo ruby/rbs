bccc08426dc4e1b3aeb41d7410ce9fb9c4b52b94
Seiei Miyagi <hanachin@gmail.com>
2019-11-06 01:35:23 +0900

    Add some test for BasicObject signature

--- test/stdlib/BasicObject_test.rb
+++ test/stdlib/BasicObject_test.rb
@@ -2,7 +2,30 @@ class BasicObjectTest < StdlibTest
   target BasicObject
   using hook.refinement
 
+  def test_not
+    BasicObject.new.!
+  end
+
+  def test_not_equal
+    BasicObject.new.!=(1)
+  end
+
+  def test_equal
+    BasicObject.new.==(1)
+    BasicObject.new.equal?(1)
+  end
+
+  def test___id__
+    BasicObject.new.__id__
+  end
+
+  def test___send__
+    BasicObject.new.__send__(:__id__)
+    BasicObject.new.__send__('__send__', :__id__)
+  end
+
   def test_instance_eval
+    BasicObject.new.instance_eval('__id__', 'filename', 1)
     BasicObject.new.instance_eval { |x| x }
   end
 
