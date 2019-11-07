23d4f2437c3a0a001323b3ca4c673445de2fb717
Seiei Miyagi <hanachin@gmail.com>
2019-11-08 08:54:26 +0900

    Make Enumerable#select into alias of Enumerable#find_all

--- test/stdlib/Enumerable_test.rb
+++ test/stdlib/Enumerable_test.rb
@@ -2,6 +2,11 @@ class EnumerableTest < StdlibTest
   target Enumerable
   using hook.refinement
 
+  def test_find_all
+    [1, 2, 3].find_all
+    [1, 2, 3].find_all { |x| x.even? }
+  end
+
   def test_grep
     [1, 2, 3].grep(-> x { x.even? })
     [1, 2, 3].grep(-> x { x.even? }) { |x| x * 2 }
@@ -10,5 +15,10 @@ class EnumerableTest < StdlibTest
   def test_grep_v
     [1, 2, 3].grep_v(-> x { x.even? })
     [1, 2, 3].grep_v(-> x { x.even? }) { |x| x * 2 }
+  end
+
+  def test_select
+    [1, 2, 3].select
+    [1, 2, 3].select { |x| x.even? }
   end
 end
