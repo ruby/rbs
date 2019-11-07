415d5fc31ed2283414e7521ad38b42e95f09086e
Seiei Miyagi <hanachin@gmail.com>
2019-11-08 08:43:03 +0900

    Add Enumerable#grep test

--- test/stdlib/Enumerable_test.rb
+++ test/stdlib/Enumerable_test.rb
@@ -2,6 +2,11 @@ class EnumerableTest < StdlibTest
   target Enumerable
   using hook.refinement
 
+  def test_grep
+    [1, 2, 3].grep(-> x { x.even? })
+    [1, 2, 3].grep(-> x { x.even? }) { |x| x * 2 }
+  end
+
   def test_grep_v
     [1, 2, 3].grep_v(-> x { x.even? })
     [1, 2, 3].grep_v(-> x { x.even? }) { |x| x * 2 }
