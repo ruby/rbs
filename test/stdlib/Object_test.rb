993dda8485d1cc39d77b2f52aed5cdecce6e93ee
Kunihiko Ito <kuni.110.92@gmail.com>
2019-11-10 16:59:12 +0900

    Add Object#respond_to?

---
+++ test/stdlib/Object_test.rb
@@ -0,0 +1,10 @@
+class ObjectTest < StdlibTest
+  target Object
+  using hook.refinement
+
+  def test_respond_to?
+    Object.new.respond_to?(:to_s)
+    Object.new.respond_to?('to_s')
+    Object.new.respond_to?('to_s', true)
+  end
+end
