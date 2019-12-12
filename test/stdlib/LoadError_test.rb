81605d061509e4ea0aaa0c1e983318c90e78d877
Seiei Miyagi <hanachin@gmail.com>
2019-12-12 23:38:13 +0900

    Add LoadError#path signature

---
+++ test/stdlib/LoadError_test.rb
@@ -0,0 +1,18 @@
+require_relative "test_helper"
+require "tempfile"
+
+class LoadErrorTest < StdlibTest
+  target LoadError
+  using hook.refinement
+
+  def test_path
+    Tempfile.open(['ruby-signature', 'rb']) do |tf|
+      path = tf.path
+      tf.unlink
+      require path
+      raise
+    rescue LoadError => exception
+      exception.path
+    end
+  end
+end
