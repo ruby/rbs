8359a2901e4dbf6c989dbe156e122bdc5b6033eb
Seiei Miyagi <hanachin@gmail.com>
2019-12-18 00:22:24 +0900

    Add FrozenError#initialize signature

--- test/stdlib/FrozenError_test.rb
+++ test/stdlib/FrozenError_test.rb
@@ -4,6 +4,12 @@ class FrozenErrorTest < StdlibTest
   target FrozenError
   using hook.refinement
 
+  def test_initialize
+    FrozenError.new
+    FrozenError.new('')
+    FrozenError.new('', 42)
+  end
+
   def test_receiver
     begin
       ''.freeze.strip!
