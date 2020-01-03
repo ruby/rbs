204ca5e67f252d6c69b5f75799e47144ff17657b
Seiei Miyagi <hanachin@gmail.com>
2020-01-03 23:54:17 +0900

    Rename NilClassTest method name

--- test/stdlib/NilClass_test.rb
+++ test/stdlib/NilClass_test.rb
@@ -4,15 +4,15 @@ class NilClassTest < StdlibTest
   target NilClass
   using hook.refinement
 
+  def test_and
+    nil & true
+  end
+
   def test_to_i
     nil.to_i
   end
 
   def test_nil?
     nil.nil?
-  end
-
-  define_method "test_&" do
-    nil & true
   end
 end
