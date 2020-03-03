55ca7cae82fbbe739d68f52e90a0bf83c5643839
aikyo <aikyo02@gmail.com>
2020-03-04 03:09:28 +0900

    Update File.absolute_path signature

---
+++ test/stdlib/File_test.rb
@@ -0,0 +1,23 @@
+require_relative "test_helper"
+require "ruby/signature/test/test_helper"
+
+class FileSingletonTest < Minitest::Test
+  include Ruby::Signature::Test::TypeAssertions
+
+  testing "singleton(::File)"
+
+  def test_absolute_path
+    assert_send_type "(String) -> String",
+                     File, :absolute_path, __FILE__
+    assert_send_type "(ToStr) -> String",
+                     File, :absolute_path, ToStr.new(__FILE__)
+    assert_send_type "(ToPath) -> String",
+                     File, :absolute_path, ToPath.new(__FILE__)
+    assert_send_type "(String, String) -> String",
+                     File, :absolute_path, __FILE__, __dir__
+    assert_send_type "(String, ToStr) -> String",
+                     File, :absolute_path, __FILE__, ToStr.new(__dir__)
+    assert_send_type "(String, ToPath) -> String",
+                     File, :absolute_path, __FILE__, ToPath.new(__dir__)
+  end
+end
