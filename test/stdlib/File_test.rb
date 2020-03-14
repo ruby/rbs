8003639b687368e2c61ada9318c7ae4fb704fb25
aikyo <aikyo02@gmail.com>
2020-03-15 04:26:13 +0900

    Update File.path signature

--- test/stdlib/File_test.rb
+++ test/stdlib/File_test.rb
@@ -417,4 +417,13 @@ class FileSingletonTest < Minitest::Test
     assert_send_type "(IO) -> bool",
                      File, :owned?, IO.new(IO.sysopen(__FILE__))
   end
+
+  def test_path
+    assert_send_type "(String) -> String",
+                     File, :path, __FILE__
+    assert_send_type "(ToStr) -> String",
+                     File, :path, ToStr.new(__FILE__)
+    assert_send_type "(ToPath) -> String",
+                     File, :path, ToPath.new(__FILE__)
+  end
 end
