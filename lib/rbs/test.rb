2bf1071cb9a7ddd3f57b8b0905e8e9c1d89a45ac
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-08-08 19:27:03 +0900

    Remove unused constants
    They are no longer used since 2a5ab09eb3d5ad40d993462e08e3a82a1b6689c3

--- lib/rbs/test.rb
+++ lib/rbs/test.rb
@@ -10,13 +10,6 @@ require "rbs/test/setup_helper"
 module RBS
   module Test
     IS_AP = Kernel.instance_method(:is_a?)
-    DEFINE_METHOD = Module.instance_method(:define_method)
-    INSTANCE_EVAL = BasicObject.instance_method(:instance_eval)
-    INSTANCE_EXEC = BasicObject.instance_method(:instance_exec)
-    METHOD = Kernel.instance_method(:method)
-    CLASS = Kernel.instance_method(:class)
-    SINGLETON_CLASS = Kernel.instance_method(:singleton_class)
-    PP = Kernel.instance_method(:pp)
     INSPECT = Kernel.instance_method(:inspect)
     METHODS = Kernel.instance_method(:methods)
 
