768cfa6462f073bfda404a9c10cb7eb1fbdd5e19
Soutaro Matsumoto <soutaro@squareup.com>
2020-09-02 14:48:38 +0900

    Allow runnint stdlib test after Ruby 2.7
    Set `FORCE_STDLIB_TEST` environment variable to run test even without Ruby 2.7.

--- bin/test_runner.rb
+++ bin/test_runner.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby
 
-RUBY_27 = Gem::Version.new(RUBY_VERSION).yield_self do |ruby_version|
-  Gem::Version.new('2.7.0') <= ruby_version && ruby_version < Gem::Version.new("2.8.0")
+$LOAD_PATH << File.join(__dir__, "../lib")
+
+STDLIB_TEST = Gem::Version.new(RUBY_VERSION).yield_self do |ruby_version|
+  Gem::Version.new('2.7.0') <= ruby_version &&
+    ruby_version <= Gem::Version.new('2.8.0')
 end
 
-unless RUBY_27
-  STDERR.puts "🚨🚨🚨 stdlib test requires Ruby 2.7 but RUBY_VERSION==#{RUBY_VERSION}, exiting... 🚨🚨🚨"
-  exit
+unless STDLIB_TEST
+  unless ENV["FORCE_STDLIB_TEST"]
+    STDERR.puts "🚨🚨🚨 stdlib test requires Ruby 2.7 or later but RUBY_VERSION==#{RUBY_VERSION}, exiting... 🚨🚨🚨"
+    exit
+  end
 end
 
 ARGV.each do |arg|
