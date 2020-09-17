ef884fe6fc02134b729efd536b2e3b522693dd67
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-09-17 11:36:02 +0900

    Update PTY signature

--- test/stdlib/PTY_test.rb
+++ test/stdlib/PTY_test.rb
@@ -4,39 +4,43 @@ require "pty"
 class PTYSingletonTest < Minitest::Test
   include TypeAssertions
 
-   library "pty"
+  library "pty"
   testing "singleton(::PTY)"
-
 
   def test_open
     assert_send_type  "() -> [ ::IO, ::File ]",
                       PTY, :open
-    assert_send_type  "() { ([ ::IO, ::File ]) -> untyped } -> untyped",
+    assert_send_type  "() { ([ ::IO, ::File ]) -> ::Integer } -> ::Integer",
                       PTY, :open do |master, slave| 1 end
   end
 
   def test_check
-    r, w, pid = PTY.spawn("echo")
+    r, w, pid = PTY.spawn("sleep 5")
+
     assert_send_type  "(::Integer pid) -> (::Process::Status | nil)",
                       PTY, :check, pid
-    assert_send_type  "(::Integer pid, ::FalseClass raise) -> (::Process::Status | nil)",
+    assert_send_type  "(::Integer pid, ::FalseClass raise) -> nil",
                       PTY, :check, pid, false
     assert_send_type  "(::Integer pid, ::TrueClass raise) -> nil",
                       PTY, :check, pid, true
+
+    Process.kill(:INT, pid)
+    Process.waitpid(pid)
   end
 
   def test_getpty
     assert_send_type  "(*::String command) -> [ ::IO, ::IO, ::Integer ]",
                       PTY, :getpty, "echo"
-    assert_send_type  "(*String command) { ([ ::IO, ::IO, ::Integer ]) -> untyped } -> untyped",
+    assert_send_type  "(*String command) { ([ ::IO, ::IO, ::Integer ]) -> ::Integer } -> nil",
                       PTY, :getpty, "echo" do |r, w, pid| 1 end
   end
 
   def test_spawn
-    assert_send_type  "(*::String command) -> [ ::IO, ::IO, ::Integer ]",
-                      PTY, :spawn, "echo"
-    assert_send_type  "(*::String command) { ([ ::IO, ::IO, ::Integer ]) -> untyped } -> untyped",
-                      PTY, :spawn, "echo" do |r, w, pid| 1 end
+    _, _, pid = assert_send_type "(*::String command) -> [ ::IO, ::IO, ::Integer ]",
+                                 PTY, :spawn, "echo"
+    Process.waitpid(pid)
+
+    assert_send_type "(*::String command) { ([ ::IO, ::IO, ::Integer ]) -> ::Integer } -> nil",
+                     PTY, :spawn, "echo" do |r, w, pid| 1 end
   end
 end
-
