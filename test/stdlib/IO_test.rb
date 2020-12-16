565c760e03bd7b86193c798e3649018033ce4b4c
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-12-17 01:33:57 +0900

    Improve IO#read, IO#readpartial, IO#write and IO.copy_stream
    This change aims to improve the following method signatures of the `IO` class:
    
    - `#read`
    - `#readpartial`
    - `#write`
    - `.copy_stream`

--- test/stdlib/IO_test.rb
+++ test/stdlib/IO_test.rb
@@ -29,4 +29,94 @@ class IOSingletonTest < Minitest::Test
                        IO, :binwrite, filename, content, 0, mode: "a"
     end
   end
+
+  def test_copy_stream
+    Dir.mktmpdir do |dir|
+      src_name = File.join(dir, "src_file").tap { |f| IO.write(f, "foo") }
+      dst_name = File.join(dir, "dst_file")
+
+      assert_send_type "(String, String) -> Integer",
+                       IO, :copy_stream, src_name, dst_name
+      assert_send_type "(String, String, Integer) -> Integer",
+                       IO, :copy_stream, src_name, dst_name, 1
+      assert_send_type "(String, String, Integer, Integer) -> Integer",
+                       IO, :copy_stream, src_name, dst_name, 1, 0
+
+      File.open(dst_name, "w") do |dst_io|
+        assert_send_type "(String, IO) -> Integer",
+                         IO, :copy_stream, src_name, dst_io
+        assert_send_type "(String, IO, Integer) -> Integer",
+                         IO, :copy_stream, src_name, dst_io, 1
+        assert_send_type "(String, IO, Integer, Integer) -> Integer",
+                         IO, :copy_stream, src_name, dst_io, 1, 0
+      end
+
+      File.open(src_name) do |src_io|
+        assert_send_type "(IO, String) -> Integer",
+                         IO, :copy_stream, src_io, dst_name
+        assert_send_type "(IO, String, Integer) -> Integer",
+                         IO, :copy_stream, src_io, dst_name, 1
+        assert_send_type "(IO, String, Integer, Integer) -> Integer",
+                         IO, :copy_stream, src_io, dst_name, 1, 0
+      end
+
+      File.open(src_name) do |src_io|
+        File.open(dst_name, "w") do |dst_io|
+          assert_send_type "(IO, IO) -> Integer",
+                           IO, :copy_stream, src_io, dst_io
+          assert_send_type "(IO, IO, Integer) -> Integer",
+                           IO, :copy_stream, src_io, dst_io, 1
+          assert_send_type "(IO, IO, Integer, Integer) -> Integer",
+                           IO, :copy_stream, src_io, dst_io, 1, 0
+        end
+      end
+    end
+  end
+end
+
+class IOInstanceTest < Minitest::Test
+  include TypeAssertions
+
+  testing "::IO"
+
+  def test_read
+    IO.open(IO.sysopen(__FILE__)) do |io|
+      assert_send_type "() -> String",
+                       io, :read
+      assert_send_type "(Integer) -> String",
+                       io, :read, 0
+      assert_send_type "(Integer) -> nil",
+                       io, :read, 1
+      assert_send_type "(nil) -> String",
+                       io, :read, nil
+      assert_send_type "(Integer, String) -> String",
+                       io, :read, 0, "buffer"
+      assert_send_type "(Integer, String) -> nil",
+                       io, :read, 1, "buffer"
+      assert_send_type "(nil, String) -> String",
+                       io, :read, nil, "buffer"
+    end
+  end
+
+  def test_readpartial
+    IO.open(IO.sysopen(__FILE__)) do |io|
+      assert_send_type "(Integer) -> String",
+                       io, :readpartial, 10
+      assert_send_type "(Integer, String) -> String",
+                       io, :readpartial, 10, "buffer"
+    end
+  end
+
+  def test_write
+    Dir.mktmpdir do |dir|
+      File.open(File.join(dir, "some_file"), "w") do |io|
+        assert_send_type "() -> Integer",
+                         io, :write
+        assert_send_type "(String) -> Integer",
+                         io, :write, "foo"
+        assert_send_type "(String, Float) -> Integer",
+                         io, :write, "foo", 1.5
+      end
+    end
+  end
 end
