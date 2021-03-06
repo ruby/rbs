1f172b28a0dcc7c66b3308ab1036af46c8ea41f8
Soutaro Matsumoto <matsumoto@soutaro.com>
2021-03-06 21:33:54 +0900

    Fix type checker

--- lib/rbs/test/type_check.rb
+++ lib/rbs/test/type_check.rb
@@ -5,13 +5,17 @@ module RBS
       attr_reader :builder
       attr_reader :sample_size
       attr_reader :unchecked_classes
+      attr_reader :instance_class
+      attr_reader :class_class
 
       attr_reader :const_cache
 
       DEFAULT_SAMPLE_SIZE = 100
 
-      def initialize(self_class:, builder:, sample_size:, unchecked_classes:)
+      def initialize(self_class:, builder:, sample_size:, unchecked_classes:, instance_class: Object, class_class: Module)
         @self_class = self_class
+        @instance_class = instance_class
+        @class_class = class_class
         @builder = builder
         @sample_size = sample_size
         @unchecked_classes = unchecked_classes.uniq
@@ -237,9 +241,9 @@ module RBS
         when Types::Bases::Nil
           Test.call(val, IS_AP, ::NilClass)
         when Types::Bases::Class
-          Test.call(val, IS_AP, Class)
+          Test.call(val, IS_AP, class_class)
         when Types::Bases::Instance
-          Test.call(val, IS_AP, self_class)
+          Test.call(val, IS_AP, instance_class)
         when Types::ClassInstance
           klass = get_class(type.name) or return false
           case
