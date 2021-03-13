5030f93961f780b3e88fade62b6ea4355a9e8392
Kenichi Kamiya <kachick1@gmail.com>
2021-03-13 23:36:07 +0900

    Add Hash#except signature

--- test/stdlib/Hash_test.rb
+++ test/stdlib/Hash_test.rb
@@ -185,6 +185,11 @@ class HashTest < StdlibTest
     { a: 2 }.eql?({ a: 1 })
   end
 
+  def test_except
+    { a: 100, b: 200, c: 300 }.except(:a)
+    { a: 100, b: 200, c: 300 }.except(:b, :c, :d)
+  end
+
   def test_fetch
     hash = { a: 1 }
     hash.fetch(:a)
@@ -371,5 +376,18 @@ class HashTest < StdlibTest
     Hash.new
     Hash.new(10)
     Hash.new { |hash, key| key.to_s }
+  end
+end
+
+class HashInstanceTest < Test::Unit::TestCase
+  include TypeAssertions
+
+  testing "::Hash[::Symbol, ::Integer]"
+
+  def test_except
+    assert_send_type "() -> ::Hash[::Symbol, ::Integer]",
+                      { a: 100, b: 200, c: 300 }, :except
+    assert_send_type "(*Symbol keys) -> ::Hash[::Symbol, ::Integer]",
+                      { a: 100, b: 200, c: 300 }, :except, :a
   end
 end
