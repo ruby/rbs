904596eed2e1ce1de8939b79f815c353bbba2ea0
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2021-03-14 19:29:01 +0900

    Skip installation if already exist

--- lib/rbs/collection/installer.rb
+++ lib/rbs/collection/installer.rb
@@ -3,6 +3,8 @@ module RBS
     class Installer
       attr_reader :lockfile
 
+      METADATA_FILENAME = '.rbs_meta.yaml'
+
       def initialize(lockfile_path:)
         @lockfile = Config.from_path(lockfile_path)
       end
@@ -10,9 +12,23 @@ module RBS
       def install_from_lockfile
         install_to = lockfile.path
         lockfile.gems.each do |config_entry|
-          RBS.logger.info "Installing #{config_entry['name']}"
-          # TODO: more log
-          collection_for(config_entry).install(install_to, config_entry)
+          gem_name = config_entry['name']
+          version = config_entry['version']
+          gem_dir = install_to.join(gem_name, version)
+
+          if gem_dir.directory?
+            if (prev = YAML.load_file(gem_dir.join(METADATA_FILENAME))) == config_entry
+              RBS.logger.info "Using #{format_config_entry(config_entry)}"
+            else
+              # @type var prev: RBS::Collection::Config::gem_entry
+              RBS.logger.info "Updating to #{format_config_entry(config_entry)} from #{format_config_entry(prev)}"
+              gem_dir.rmtree
+              collection_for(config_entry).install(install_to, config_entry)
+            end
+          else
+            RBS.logger.info "Installing #{format_config_entry(config_entry)}"
+            collection_for(config_entry).install(install_to, config_entry)
+          end
         end
       end
 
@@ -20,6 +36,17 @@ module RBS
         @collection_for ||= {}
         key = config_entry['collection']
         @collection_for[key] ||= Collections.from_config_entry(key)
+      end
+
+      # TODO: Support non git collection such as stdlib
+      private def format_config_entry(config_entry)
+        name = config_entry['name']
+        v = config_entry['version']
+        rev = config_entry['revision']
+        # shorten sha1 commit hash
+        rev = rev[0..10] if /\A[a-f0-9]{40}\z/.match? rev
+
+        "#{name}:#{v} (#{rev})"
       end
     end
   end
