a453ea9ac42d36d6acfc36f3cb73aa445102322f
Soutaro Matsumoto <matsumoto@soutaro.com>
2021-03-31 22:01:49 +0900

    Expand #first

--- lib/rbs/constant_table.rb
+++ lib/rbs/constant_table.rb
@@ -56,7 +56,10 @@ module RBS
                         name_to_constant(TypeName.new(name: head, namespace: Namespace.root))
                       else
                         resolve_constant_reference_context(head, context: context) ||
-                          resolve_constant_reference_inherit(head, scopes: constant_scopes(context.first.to_type_name))
+                          context.first.yield_self do |first_contet|
+                            raise unless first_contet
+                            resolve_constant_reference_inherit(head, scopes: constant_scopes(first_contet.to_type_name))
+                          end
                       end
 
       tail.inject(head_constant) do |constant, name|
