e2a46858a52c77f17acba0a386856ad87dea453d
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2021-06-25 00:00:19 +0900

    refactor

--- lib/rbs/collection.rb
+++ lib/rbs/collection.rb
@@ -3,6 +3,7 @@ require 'bundler'
 
 require_relative './collection/collections'
 require_relative './collection/config'
+require_relative './collection/config/gemfile_lock_loader'
 require_relative './collection/installer'
 require_relative './collection/cleaner'
 
