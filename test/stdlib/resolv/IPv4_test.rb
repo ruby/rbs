71d2fb8b44f836db8da8f71046422cc6c959181e
HoneyryderChuck <cardoso_tiago@hotmail.com>
2021-06-28 18:09:02 +0300

    added real sigs for Resolv::IPv4 and Resolv::IPv6

---
+++ test/stdlib/resolv/IPv4_test.rb
@@ -0,0 +1,51 @@
+require_relative '../test_helper'
+require 'resolv'
+
+
+class ResolvIPv4SingletonTest < Test::Unit::TestCase
+  include TypeAssertions
+  library 'resolv'
+  testing 'singleton(::Resolv::IPv4)'
+
+  def test_create
+    assert_send_type "(String) -> Resolv::IPv4",
+      Resolv::IPv4, :create, "8.8.8.8"
+
+    rdata = [8,8,8,8].pack("CCCC")
+    ip = Resolv::IPv4.new(rdata)
+    assert_send_type "(Resolv::IPv4) -> Resolv::IPv4",
+      Resolv::IPv4, :create, ip
+  end
+end
+
+class ResolvIPv4InstanceTest < Test::Unit::TestCase
+  include TypeAssertions
+  library 'resolv'
+  testing '::Resolv::IPv4'
+
+  def ip(ipv4 = "127.0.0.1")
+    rdata = ipv4.split(".").map(&:to_i).pack("CCCC")
+    Resolv::IPv4.new(rdata)
+  end
+
+  def test_eql?
+    assert_send_type "(Resolv::IPv4) -> bool",
+      ip, :==, ip("8.8.8.8")
+  end
+
+  def test_address
+    assert_send_type "() -> String",
+      ip, :address 
+  end
+
+  def test_hash
+    assert_send_type "() -> Integer",
+      ip, :hash  
+  end
+
+  def test_to_name
+    assert_send_type "() -> Resolv::DNS::Name",
+      ip, :to_name
+  end
+
+end
---
+++ test/stdlib/resolv/IPv6_test.rb
@@ -0,0 +1,49 @@
+require_relative '../test_helper'
+require 'resolv'
+
+
+class ResolvIPv6SingletonTest < Test::Unit::TestCase
+  include TypeAssertions
+  library 'resolv'
+  testing 'singleton(::Resolv::IPv6)'
+
+  def test_create
+    assert_send_type "(String) -> Resolv::IPv6",
+      Resolv::IPv6, :create, "2001:4860:4860::8888"
+
+    ip = Resolv::IPv6.create("2001:4860:4860::8888")
+    assert_send_type "(Resolv::IPv6) -> Resolv::IPv6",
+      Resolv::IPv6, :create, ip
+  end
+end
+
+class ResolvIPv6InstanceTest < Test::Unit::TestCase
+  include TypeAssertions
+  library 'resolv'
+  testing '::Resolv::IPv6'
+
+  def ip(ipv6 = "::1")
+    Resolv::IPv6.create(ipv6)
+  end
+
+  def test_eql?
+    assert_send_type "(Resolv::IPv6) -> bool",
+      ip, :==, ip("2001:4860:4860::8888")
+  end
+
+  def test_address
+    assert_send_type "() -> String",
+      ip, :address 
+  end
+
+  def test_hash
+    assert_send_type "() -> Integer",
+      ip, :hash  
+  end
+
+  def test_to_name
+    assert_send_type "() -> Resolv::DNS::Name",
+      ip, :to_name
+  end
+
+end
