5b5ac90e105e32c5d08be44401be82e988c0d6d0
ksss <co000ri@gmail.com>
2022-03-22 18:15:18 +0900

    Reimplement testing code for prime

--- test/stdlib/Prime_test.rb
+++ test/stdlib/Prime_test.rb
@@ -1,35 +1,75 @@
 require_relative "test_helper"
 
-begin
-  require "prime"
+require "prime"
 
-  class PrimeTest < StdlibTest
-    target Prime
-    library "prime"
+class PrimeSingletonTest < Test::Unit::TestCase
+  include TypeAssertions
 
-    def test_each
-      Prime.each { break }
-      Prime.each(10) { }
-      Prime.each(100, Prime::TrialDivisionGenerator.new)
-    end
+  library "singleton", "prime"
+  testing "singleton(::Prime)"
 
-    def test_prime?
-      Prime.prime?(10)
-      Prime.prime?(11)
-    end
-
-    def test_int_from_prime_division
-      Prime.int_from_prime_division([[2, 3], [3, 4]])
-    end
-
-    def test_prime_division
-      Prime.prime_division(6)
-    end
-
-    def test_instance
-      Prime.instance.prime?(100)
-    end
+  def test_each
+    assert_send_type  "(?::Integer? ubound, ?::Prime::PseudoPrimeGenerator generator) { (::Integer) -> void } -> void",
+                      Prime, :each do break end
+    assert_send_type  "(?::Integer? ubound, ?::Prime::PseudoPrimeGenerator generator) { (::Integer) -> void } -> void",
+                      Prime, :each, 10 do break end
+    assert_send_type  "(?::Integer? ubound, ?::Prime::PseudoPrimeGenerator generator) { (::Integer) -> void } -> void",
+                      Prime, :each, 10, Prime::TrialDivisionGenerator.new do break end
+    assert_send_type  "(?::Integer? ubound, ?::Prime::PseudoPrimeGenerator generator) -> ::Prime::PseudoPrimeGenerator",
+                      Prime, :each
   end
-rescue LoadError
-  # for Ruby 3.1
+
+  def test_int_from_prime_division
+    assert_send_type  "(::Array[[ ::Integer, ::Integer ]]) -> ::Integer",
+                      Prime, :int_from_prime_division, [[3, 1], [19, 1]]
+  end
+
+  def test_prime?
+    assert_send_type  "(::Integer value, ?::Prime::PseudoPrimeGenerator generator) -> bool",
+                      Prime, :prime?, 57
+  end
+
+  def test_prime_division
+    assert_send_type  "(::Integer, ?::Prime::PseudoPrimeGenerator generator) -> ::Array[[ ::Integer, ::Integer ]]",
+                      Prime, :prime_division, 57
+  end
+
+  def test_instance
+    assert_send_type  "() -> ::Prime",
+                      Prime, :instance
+  end
+end
+
+class PrimeTest < Test::Unit::TestCase
+  include TypeAssertions
+
+  library "singleton", "prime"
+  testing "::Prime"
+
+
+  def test_each
+    assert_send_type  "(?::Integer? ubound, ?::Prime::PseudoPrimeGenerator generator) { (::Integer) -> void } -> void",
+                      Prime.instance, :each do break end
+    assert_send_type  "(?::Integer? ubound, ?::Prime::PseudoPrimeGenerator generator) { (::Integer) -> void } -> void",
+                      Prime.instance, :each, 10 do break end
+    assert_send_type  "(?::Integer? ubound, ?::Prime::PseudoPrimeGenerator generator) { (::Integer) -> void } -> void",
+                      Prime.instance, :each, 10, Prime::TrialDivisionGenerator.new do break end
+    assert_send_type  "(?::Integer? ubound, ?::Prime::PseudoPrimeGenerator generator) -> ::Prime::PseudoPrimeGenerator",
+                      Prime.instance, :each
+  end
+
+  def test_int_from_prime_division
+    assert_send_type  "(::Array[[ ::Integer, ::Integer ]]) -> ::Integer",
+                      Prime.instance, :int_from_prime_division, [[3, 1], [19, 1]]
+  end
+
+  def test_prime?
+    assert_send_type  "(::Integer value, ?::Prime::PseudoPrimeGenerator generator) -> bool",
+                      Prime.instance, :prime?, 57
+  end
+
+  def test_prime_division
+    assert_send_type  "(::Integer, ?::Prime::PseudoPrimeGenerator generator) -> ::Array[[ ::Integer, ::Integer ]]",
+                      Prime.instance, :prime_division, 57
+  end
 end
