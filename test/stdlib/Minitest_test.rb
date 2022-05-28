8e335c95d84cf66541bef465976029e508cd8bc4
ksss <co000ri@gmail.com>
2022-05-28 23:50:05 +0900

    Add Minitest to stdlib
    All definitions were generated by following script.
    https://github.com/ksss/orthoses/blob/2d6692d4584a30d4008cea06fc54bfee3a102b35/examples/minitest/Rakefile

---
+++ test/stdlib/Minitest_test.rb
@@ -0,0 +1,40 @@
+require_relative "test_helper"
+require 'minitest'
+
+class MinitestSingletonTest < Test::Unit::TestCase
+  include TypeAssertions
+
+  library "minitest"
+  testing "singleton(::Minitest)"
+
+  def test_clock_time
+    assert_send_type  "() -> untyped",
+                      Minitest, :clock_time
+  end
+
+  def test_filter_backtrace
+    assert_send_type  "(untyped bt) -> untyped",
+                      Minitest, :filter_backtrace, caller
+  end
+
+  def test_process_args
+    assert_send_type  "(?untyped args) -> untyped",
+                      Minitest, :process_args
+  end
+
+  def test_load_plugins
+    assert_send_type  "() -> (nil | untyped)",
+                      Minitest, :load_plugins
+  end
+
+  def test_init_plugins
+    assert_send_type  "(untyped options) -> untyped",
+                      Minitest, :init_plugins, {}
+  end
+
+  def test_after_run
+    assert_send_type  "() { () -> untyped } -> untyped",
+                      Minitest, :after_run do end
+  end
+end
+
