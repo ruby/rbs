f71aded06b6be30984c0992cdce9f18f6c3fba5c
Soutaro Matsumoto <matsumoto@soutaro.com>
2022-07-22 16:49:13 +0900

    Adding unit tests
    * Refactored RDoc::Parser::RBS to make unit testing easier
    * Adding some test cases
    
    Co-authored-by: Sushanth Sathesh Rao <raosush@users.noreply.github.com>

--- lib/rdoc/parser/rbs.rb
+++ lib/rdoc/parser/rbs.rb
@@ -2,152 +2,167 @@
 
 require 'rbs'
 
-class RDoc::Parser::RBS < RDoc::Parser
-  parse_files_matching(/\.rbs$/)
+module RDoc
+  class Parser
+    class RBS < Parser
+      parse_files_matching(/\.rbs$/)
 
-  def initialize(top_level, file_name, content, options, stats)
-    super
-  end
-
-  def scan
-    ast = ::RBS::Parser.parse_signature(@content)
-    ast.each do |decl|
-      parse_member(decl: decl, context: @top_level)
-    end
-    @top_level
-  end
-
-  def parse_member(decl:, context:, outer_name: nil)
-    case decl
-    when ::RBS::AST::Declarations::Class
-      parse_class_decl(decl: decl, context: context, outer_name: outer_name)
-    when ::RBS::AST::Declarations::Module
-      parse_module_decl(decl: decl, context: context, outer_name: outer_name)
-    when ::RBS::AST::Declarations::Constant
-      context = @top_level.find_class_or_module outer_name.to_s if outer_name
-      parse_constant_decl(decl: decl, context: context, outer_name: outer_name)
-    when ::RBS::AST::Declarations::Interface
-      parse_module_decl(decl: decl, context: context, outer_name: outer_name)
-    when ::RBS::AST::Members::MethodDefinition
-      context = @top_level.find_class_or_module outer_name.to_s if outer_name
-      parse_method_decl(decl: decl, context: context, outer_name: outer_name)
-    when ::RBS::AST::Members::Alias
-      context = @top_level.find_class_or_module outer_name.to_s if outer_name
-      parse_method_alias_decl(decl: decl, context: context, outer_name: outer_name)
-    when ::RBS::AST::Members::AttrReader, ::RBS::AST::Members::AttrWriter, ::RBS::AST::Members::AttrAccessor
-      context = @top_level.find_class_or_module outer_name.to_s if outer_name
-      parse_attr_decl(decl: decl, context: context, outer_name: outer_name)
-    when ::RBS::AST::Members::Include
-      context = @top_level.find_class_or_module outer_name.to_s if outer_name
-      parse_include_decl(decl: decl, context: context, outer_name: outer_name)
-    when ::RBS::AST::Members::Extend
-      context = @top_level.find_class_or_module outer_name.to_s if outer_name
-      parse_extend_decl(decl: decl, context: context, outer_name: outer_name)
-    end
-  end
-
-  def parse_class_decl(decl:, context:, outer_name: nil)
-    full_name = fully_qualified_name(outer_name: outer_name, decl: decl)
-    klass = context.add_class(RDoc::NormalClass, full_name.to_s, decl.super_class&.name&.to_s || "::Object")
-    klass.add_comment(construct_comment(context: context, comment: decl.comment.string), context) if decl.comment
-    decl.members.each { |member| parse_member(decl: member, context: context, outer_name: full_name) }
-  end
-
-  def parse_module_decl(decl:, context:, outer_name: nil)
-    full_name = fully_qualified_name(outer_name: outer_name, decl: _ = decl)
-    kmodule = context.add_module(RDoc::NormalModule, full_name.to_s)
-    # @type var comment: RBS::AST::Comment
-    kmodule.add_comment(construct_comment(context: context, comment: comment.string), context) if (comment = decl.comment)
-    decl.members.each { |member| parse_member(decl: member, context: context, outer_name: full_name) }
-  end
-
-  def parse_constant_decl(decl:, context:, outer_name: nil)
-    comment = decl.comment ? construct_comment(context: context, comment: decl.comment.string) : nil
-    constant = RDoc::Constant.new(decl.name.to_s, decl.type.to_s, comment)
-    context.add_constant(constant)
-  end
-
-  def parse_method_decl(decl:, context:, outer_name: nil)
-    method = RDoc::AnyMethod.new(nil, decl.name.to_s)
-    method.singleton = decl.singleton?
-    method.visibility = decl.visibility
-    method.call_seq = decl.types.map { |type| "#{decl.name.to_s}#{type.to_s}" }.join("\n")
-    if decl.location
-      method.start_collecting_tokens
-      method.add_token({ line_no: 1, char_no: 1, kind: :on_comment, text: "# File #{@top_level.relative_name}, line(s) #{decl.location.start_line}:#{decl.location.end_line}\n" })
-      method.add_token({ line_no: 1, char_no: 1, text: decl.location.source })
-      method.line = decl.location.start_line if decl.location
-    end
-    method.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
-    context.add_method(method)
-  end
-
-  def parse_method_alias_decl(decl:, context:, outer_name: nil)
-    alias_def = RDoc::Alias.new(nil, decl.old_name.to_s, decl.new_name.to_s, nil, decl.kind == :singleton)
-    alias_def.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
-    context.add_alias(alias_def)
-  end
-
-  def parse_attr_decl(decl:, context:, outer_name: nil)
-    rw = case decl
-         when ::RBS::AST::Members::AttrReader
-           'R'
-         when ::RBS::AST::Members::AttrWriter
-           'W'
-         when ::RBS::AST::Members::AttrAccessor
-           'RW'
-         end
-    attribute = RDoc::Attr.new(nil, decl.name.to_s, rw, nil, decl.kind == :singleton)
-    attribute.visibility = decl.visibility
-    attribute.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
-    context.add_attribute(attribute)
-  end
-
-  def parse_include_decl(decl:, context:, outer_name: nil)
-    name = decl.name.to_s
-    outer_names = outer_name ? outer_name.to_s.split("::") : []
-    qualified_name = ''
-    outer_names.each do |namespace|
-      qualified_name += namespace
-      if (module_name = @top_level.find_module_named((qualified_name += "::") + name))
-        name = module_name.full_name
-        break
+      def scan
+        RBS::RDocPlugin::RBSParser.new(@top_level, @content).scan
       end
     end
-    include_decl = RDoc::Include.new(name, nil)
-    include_decl.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
-    context.add_include(include_decl)
   end
+end
 
-  def parse_extend_decl(decl:, context:, outer_name: nil)
-    name = decl.name.to_s
-    outer_names = outer_name ? outer_name.to_s.split("::") : []
-    qualified_name = ''
-    outer_names.each do |namespace|
-      qualified_name += namespace
-      if (module_name = @top_level.find_module_named((qualified_name += "::") + name))
-        name = module_name.full_name
-        break
+module RBS
+  module RDocPlugin
+    class RBSParser
+      attr_reader :top_level, :content
+      def initialize(top_level, content)
+        @top_level = top_level
+        @content = content
       end
-    end
-    extend_decl = RDoc::Extend.new(name, nil)
-    extend_decl.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
-    context.add_extend(extend_decl)
-  end
 
-  private
+      def scan
+        ast = ::RBS::Parser.parse_signature(@content)
+        ast.each do |decl|
+          parse_member(decl: decl, context: @top_level)
+        end
+        @top_level
+      end
 
-  def construct_comment(context:, comment:)
-    comment = RDoc::Comment.new(comment, context)
-    comment.format = "markdown"
-    comment
-  end
+      def parse_member(decl:, context:, outer_name: nil)
+        case decl
+        when ::RBS::AST::Declarations::Class
+          parse_class_decl(decl: decl, context: context, outer_name: outer_name)
+        when ::RBS::AST::Declarations::Module
+          parse_module_decl(decl: decl, context: context, outer_name: outer_name)
+        when ::RBS::AST::Declarations::Constant
+          context = @top_level.find_class_or_module outer_name.to_s if outer_name
+          parse_constant_decl(decl: decl, context: context, outer_name: outer_name)
+        when ::RBS::AST::Declarations::Interface
+          parse_module_decl(decl: decl, context: context, outer_name: outer_name)
+        when ::RBS::AST::Members::MethodDefinition
+          context = @top_level.find_class_or_module outer_name.to_s if outer_name
+          parse_method_decl(decl: decl, context: context, outer_name: outer_name)
+        when ::RBS::AST::Members::Alias
+          context = @top_level.find_class_or_module outer_name.to_s if outer_name
+          parse_method_alias_decl(decl: decl, context: context, outer_name: outer_name)
+        when ::RBS::AST::Members::AttrReader, ::RBS::AST::Members::AttrWriter, ::RBS::AST::Members::AttrAccessor
+          context = @top_level.find_class_or_module outer_name.to_s if outer_name
+          parse_attr_decl(decl: decl, context: context, outer_name: outer_name)
+        when ::RBS::AST::Members::Include
+          context = @top_level.find_class_or_module outer_name.to_s if outer_name
+          parse_include_decl(decl: decl, context: context, outer_name: outer_name)
+        when ::RBS::AST::Members::Extend
+          context = @top_level.find_class_or_module outer_name.to_s if outer_name
+          parse_extend_decl(decl: decl, context: context, outer_name: outer_name)
+        end
+      end
 
-  def fully_qualified_name(outer_name:, decl:)
-    if outer_name
-      (outer_name + decl.name)
-    else
-      decl.name
+      def parse_class_decl(decl:, context:, outer_name: nil)
+        full_name = fully_qualified_name(outer_name: outer_name, decl: decl)
+        klass = context.add_class(RDoc::NormalClass, full_name.to_s, decl.super_class&.name&.to_s || "::Object")
+        klass.add_comment(construct_comment(context: context, comment: decl.comment.string), context) if decl.comment
+        decl.members.each { |member| parse_member(decl: member, context: context, outer_name: full_name) }
+      end
+
+      def parse_module_decl(decl:, context:, outer_name: nil)
+        full_name = fully_qualified_name(outer_name: outer_name, decl: _ = decl)
+        kmodule = context.add_module(RDoc::NormalModule, full_name.to_s)
+        kmodule.add_comment(construct_comment(context: context, comment: decl.comment.string), context) if decl.comment
+        decl.members.each { |member| parse_member(decl: member, context: context, outer_name: full_name) }
+      end
+
+      def parse_constant_decl(decl:, context:, outer_name: nil)
+        comment = decl.comment ? construct_comment(context: context, comment: decl.comment.string) : nil
+        constant = RDoc::Constant.new(decl.name.to_s, decl.type.to_s, comment)
+        context.add_constant(constant)
+      end
+
+      def parse_method_decl(decl:, context:, outer_name: nil)
+        method = RDoc::AnyMethod.new(nil, decl.name.to_s)
+        method.singleton = decl.singleton?
+        method.visibility = decl.visibility
+        method.call_seq = decl.types.map { |type| "#{decl.name.to_s}#{type.to_s}" }.join("\n")
+        if decl.location
+          method.start_collecting_tokens
+          method.add_token({ line_no: 1, char_no: 1, kind: :on_comment, text: "# File #{@top_level.relative_name}, line(s) #{decl.location.start_line}:#{decl.location.end_line}\n" })
+          method.add_token({ line_no: 1, char_no: 1, text: decl.location.source })
+          method.line = decl.location.start_line if decl.location
+        end
+        method.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
+        context.add_method(method)
+      end
+
+      def parse_method_alias_decl(decl:, context:, outer_name: nil)
+        alias_def = RDoc::Alias.new(nil, decl.old_name.to_s, decl.new_name.to_s, nil, decl.kind == :singleton)
+        alias_def.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
+        context.add_alias(alias_def)
+      end
+
+      def parse_attr_decl(decl:, context:, outer_name: nil)
+        rw = case decl
+             when ::RBS::AST::Members::AttrReader
+               'R'
+             when ::RBS::AST::Members::AttrWriter
+               'W'
+             when ::RBS::AST::Members::AttrAccessor
+               'RW'
+             end
+        attribute = RDoc::Attr.new(nil, decl.name.to_s, rw, nil, decl.kind == :singleton)
+        attribute.visibility = decl.visibility
+        attribute.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
+        context.add_attribute(attribute)
+      end
+
+      def parse_include_decl(decl:, context:, outer_name: nil)
+        name = decl.name.to_s
+        outer_names = outer_name ? outer_name.to_s.split("::") : []
+        qualified_name = ''
+        outer_names.each do |namespace|
+          qualified_name += namespace
+          if (module_name = @top_level.find_module_named((qualified_name += "::") + name))
+            name = module_name.full_name
+            break
+          end
+        end
+        include_decl = RDoc::Include.new(name, nil)
+        include_decl.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
+        context.add_include(include_decl)
+      end
+
+      def parse_extend_decl(decl:, context:, outer_name: nil)
+        name = decl.name.to_s
+        outer_names = outer_name ? outer_name.to_s.split("::") : []
+        qualified_name = ''
+        outer_names.each do |namespace|
+          qualified_name += namespace
+          if (module_name = @top_level.find_module_named((qualified_name += "::") + name))
+            name = module_name.full_name
+            break
+          end
+        end
+        extend_decl = RDoc::Extend.new(name, nil)
+        extend_decl.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
+        context.add_extend(extend_decl)
+      end
+
+      private
+
+      def construct_comment(context:, comment:)
+        comment = RDoc::Comment.new(comment, context)
+        comment.format = "markdown"
+        comment
+      end
+
+      def fully_qualified_name(outer_name:, decl:)
+        if outer_name
+          (outer_name + decl.name)
+        else
+          decl.name
+        end
+      end
     end
   end
 end
--- test/rbs/rdoc/rbs_parser_test.rb
+++ test/rbs/rdoc/rbs_parser_test.rb
@@ -1,20 +1,150 @@
 require "test_helper"
 
+require "tempfile"
+
+require "rdoc/parser/rbs"
+
 class RBSParserTest < Test::Unit::TestCase
   def parser(content)
-    @tempfile = Tempfile.new self.class.name
-    @filename = @tempfile.path
+    top_level = RDoc::TopLevel.new("a.rbs")
+    top_level.store = RDoc::Store.new()
 
-    RDoc::TopLevel.reset
-    @top_level = RDoc::TopLevel.new @filename
-
-    @options = RDoc::Options.new
-    @options.quiet = true
-    @stats = RDoc::Stats.new 0
-    @parser = RDoc::Parser::RBS.new(@top_level, @filename, content, @options, @stats)
+    RBS::RDocPlugin::RBSParser.new(top_level, content)
   end
 
   def teardown
-    @tempfile.close
+  end
+
+  def test_class_decl_1
+    parser = parser(<<~RBS)
+class A
+end
+RBS
+
+    parser.scan()
+
+    top_level = parser.top_level
+
+    klass = top_level.find_class_or_module("A")
+
+    assert_instance_of RDoc::NormalClass, klass
+    assert_equal "A", klass.full_name
+    assert_equal "A", klass.name
+    assert_equal "", klass.comment
+    assert_equal "Object", klass.superclass
+  end
+
+  def test_class_decl_2
+    parser = parser(<<~RBS)
+# This is class A
+class A[Store] < StringBuilder[Store]
+end
+RBS
+
+    parser.scan()
+
+    top_level = parser.top_level
+
+    klass = top_level.find_class_or_module("A")
+
+    assert_instance_of RDoc::NormalClass, klass
+    assert_equal "A", klass.full_name
+    assert_equal "A", klass.name
+    assert_equal "This is class A", klass.comment.text
+    assert_equal "StringBuilder", klass.superclass
+  end
+
+  def test_module_decl_1
+    parser = parser(<<~RBS)
+module A
+end
+RBS
+
+    parser.scan()
+
+    top_level = parser.top_level
+
+    klass = top_level.find_class_or_module("A")
+
+    assert_instance_of RDoc::NormalModule, klass
+    assert_equal "A", klass.full_name
+    assert_equal "A", klass.name
+    assert_equal "", klass.comment
+  end
+
+  def test_module_decl_2
+    parser = parser(<<~RBS)
+# Test comment for module A
+module A[Element] : _Each[Element]
+end
+RBS
+
+    parser.scan()
+
+    top_level = parser.top_level
+
+    klass = top_level.find_class_or_module("A")
+
+    assert_instance_of RDoc::NormalModule, klass
+    assert_equal "A", klass.full_name
+    assert_equal "A", klass.name
+    assert_equal "Test comment for module A", klass.comment.text
+  end
+
+  def test_instance_method_1
+    parser = parser(<<~RBS)
+class A
+  def foo: (Integer) -> void
+end
+RBS
+
+    parser.scan()
+
+    top_level = parser.top_level
+
+    klass = top_level.find_class_or_module("A")
+    method = klass.method_list.find {|m| m.name == "foo" }
+
+    assert_instance_of RDoc::AnyMethod, method
+    assert_equal "", method.comment
+    assert_equal "foo(Integer) -> void", method.call_seq
+  end
+
+  def test_instance_method_with_block
+    parser = parser(<<~RBS)
+class A
+  def foo: (Integer) { (String) -> void } -> void
+end
+RBS
+
+    parser.scan()
+
+    top_level = parser.top_level
+
+    klass = top_level.find_class_or_module("A")
+    method = klass.method_list.find {|m| m.name == "foo" }
+
+    assert_instance_of RDoc::AnyMethod, method
+    assert_equal "", method.comment
+    assert_equal "foo(Integer) { (String) -> void } -> void", method.call_seq
+  end
+
+  def test_instance_method_generic
+    parser = parser(<<~RBS)
+class A
+  def foo: [A] (Integer) { (String) -> A } -> A
+end
+RBS
+
+    parser.scan()
+
+    top_level = parser.top_level
+
+    klass = top_level.find_class_or_module("A")
+    method = klass.method_list.find {|m| m.name == "foo" }
+
+    assert_instance_of RDoc::AnyMethod, method
+    assert_equal "", method.comment
+    assert_equal "foo[A] (Integer) { (String) -> A } -> A", method.call_seq
   end
 end
