914dfc7672b86fca3e755570388d6a0d4d156e00
Soutaro Matsumoto <matsumoto@soutaro.com>
2022-07-23 23:50:00 +0900

    Type check lib/rdoc/parser/rbs.rb

--- lib/rdoc/parser/rbs.rb
+++ lib/rdoc/parser/rbs.rb
@@ -65,19 +65,19 @@ module RBS
       def parse_class_decl(decl:, context:, outer_name: nil)
         full_name = fully_qualified_name(outer_name: outer_name, decl: decl)
         klass = context.add_class(RDoc::NormalClass, full_name.to_s, decl.super_class&.name&.to_s || "::Object")
-        klass.add_comment(construct_comment(context: context, comment: decl.comment.string), context) if decl.comment
+        klass.add_comment(construct_comment(context: context, comment: comment_string(decl)), context) if decl.comment
         decl.members.each { |member| parse_member(decl: member, context: context, outer_name: full_name) }
       end
 
       def parse_module_decl(decl:, context:, outer_name: nil)
         full_name = fully_qualified_name(outer_name: outer_name, decl: _ = decl)
         kmodule = context.add_module(RDoc::NormalModule, full_name.to_s)
-        kmodule.add_comment(construct_comment(context: context, comment: decl.comment.string), context) if decl.comment
+        kmodule.add_comment(construct_comment(context: context, comment: comment_string(decl)), context) if decl.comment
         decl.members.each { |member| parse_member(decl: member, context: context, outer_name: full_name) }
       end
 
       def parse_constant_decl(decl:, context:, outer_name: nil)
-        comment = decl.comment ? construct_comment(context: context, comment: decl.comment.string) : nil
+        comment = decl.comment ? construct_comment(context: context, comment: comment_string(decl)) : nil
         constant = RDoc::Constant.new(decl.name.to_s, decl.type.to_s, comment)
         context.add_constant(constant)
       end
@@ -87,19 +87,19 @@ module RBS
         method.singleton = decl.singleton?
         method.visibility = decl.visibility
         method.call_seq = decl.types.map { |type| "#{decl.name.to_s}#{type.to_s}" }.join("\n")
-        if decl.location
+        if loc = decl.location
           method.start_collecting_tokens
-          method.add_token({ line_no: 1, char_no: 1, kind: :on_comment, text: "# File #{@top_level.relative_name}, line(s) #{decl.location.start_line}:#{decl.location.end_line}\n" })
-          method.add_token({ line_no: 1, char_no: 1, text: decl.location.source })
-          method.line = decl.location.start_line if decl.location
+          method.add_token({ line_no: 1, char_no: 1, kind: :on_comment, text: "# File #{@top_level.relative_name}, line(s) #{loc.start_line}:#{loc.end_line}\n" })
+          method.add_token({ line_no: 1, char_no: 1, text: loc.source })
+          method.line = loc.start_line
         end
-        method.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
+        method.comment = construct_comment(context: context, comment: comment_string(decl)) if decl.comment
         context.add_method(method)
       end
 
       def parse_method_alias_decl(decl:, context:, outer_name: nil)
         alias_def = RDoc::Alias.new(nil, decl.old_name.to_s, decl.new_name.to_s, nil, decl.kind == :singleton)
-        alias_def.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
+        alias_def.comment = construct_comment(context: context, comment: comment_string(decl)) if decl.comment
         context.add_alias(alias_def)
       end
 
@@ -114,7 +114,7 @@ module RBS
              end
         attribute = RDoc::Attr.new(nil, decl.name.to_s, rw, nil, decl.kind == :singleton)
         attribute.visibility = decl.visibility
-        attribute.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
+        attribute.comment = construct_comment(context: context, comment: comment_string(decl)) if decl.comment
         context.add_attribute(attribute)
       end
 
@@ -130,7 +130,7 @@ module RBS
           end
         end
         include_decl = RDoc::Include.new(name, nil)
-        include_decl.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
+        include_decl.comment = construct_comment(context: context, comment: comment_string(decl)) if decl.comment
         context.add_include(include_decl)
       end
 
@@ -146,7 +146,7 @@ module RBS
           end
         end
         extend_decl = RDoc::Extend.new(name, nil)
-        extend_decl.comment = construct_comment(context: context, comment: decl.comment.string) if decl.comment
+        extend_decl.comment = construct_comment(context: context, comment: comment_string(decl)) if decl.comment
         context.add_extend(extend_decl)
       end
 
@@ -156,6 +156,11 @@ module RBS
         comment = RDoc::Comment.new(comment, context)
         comment.format = "markdown"
         comment
+      end
+
+      def comment_string(with_comment)
+        comment = with_comment.comment or raise "Object with `#comment` returning a object is expected"
+        comment.string
       end
 
       def fully_qualified_name(outer_name:, decl:)
