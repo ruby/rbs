68eb75ee02bbd7fe7a95b79fcecbed549e988582
Soutaro Matsumoto <matsumoto@soutaro.com>
2022-11-15 11:09:15 +0900

    Let installer use Lockfile

--- lib/rbs/collection/installer.rb
+++ lib/rbs/collection/installer.rb
@@ -7,31 +7,22 @@ module RBS
       attr_reader :stdout
 
       def initialize(lockfile_path:, stdout: $stdout)
-        @lockfile = Config.from_path(lockfile_path)
+        @lockfile = Config::Lockfile.from_lockfile(lockfile_path: lockfile_path, data: YAML.load_file(lockfile_path))
         @stdout = stdout
       end
 
       def install_from_lockfile
-        install_to = lockfile.repo_path
+        install_to = lockfile.fullpath
         install_to.mkpath
-        lockfile.gems.each do |config_entry|
-          source_for(config_entry).install(
+        lockfile.gems.each_value do |gem|
+          gem[:source].install(
             dest: install_to,
-            name: config_entry["name"],
-            version: config_entry["version"] || raise,
+            name: gem[:name],
+            version: gem[:version],
             stdout: stdout
           )
         end
         stdout.puts "It's done! #{lockfile.gems.size} gems' RBSs now installed."
-      end
-
-      private def source_for(config_entry)
-        @source_for ||= {}
-        key = config_entry['source']
-        unless key
-          raise "Cannot find source of '#{config_entry['name']}' gem"
-        end
-        @source_for[key] ||= Sources.from_config_entry(key)
       end
     end
   end
