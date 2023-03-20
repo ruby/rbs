0eaa27fb0586ec21402b6a5493cbf661b4043cfe
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2023-03-20 13:33:00 +0900

    subtract

---
+++ lib/rbs/subtractor.rb
@@ -0,0 +1,12 @@
+module RBS
+  class Subtractor
+    def subtract(minuend, subtrahend)
+      minuend.select do |decl|
+        decl
+      end
+    end
+
+    private def build_substrahend_map!
+    end
+  end
+end
