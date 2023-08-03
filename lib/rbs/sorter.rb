db4c13b1a13274ad7704e8fdab18c7387c6bf738
Soutaro Matsumoto <matsumoto@soutaro.com>
2023-08-03 14:47:29 +0900

    Type check & refactor sorter

--- lib/rbs/sorter.rb
+++ lib/rbs/sorter.rb
@@ -28,140 +28,167 @@ module RBS
       end
     end
 
-    def group(member)
-      case member
-      when Declarations::TypeAlias
-        -3
-      when Declarations::Constant
-        -2
-      when Declarations::Class, Declarations::Module, Declarations::Interface
-        -1
-      when Members::Include
-        0.0
-      when Members::Prepend
-        0.2
-      when Members::Extend
-        0.4
-      when Members::ClassVariable
-        1
-      when Members::ClassInstanceVariable
-        2
-      when Members::InstanceVariable
-        3
-      when Members::AttrAccessor, Members::AttrWriter, Members::AttrReader
-        if member.kind == :singleton
-          5.0
-        else
-          6.0
-        end
-      when Members::MethodDefinition
-        case member.kind
-        when :singleton_instance
-          4
-        when :singleton
-          if member.name == :new
-            5.2
-          elsif member.visibility == :public
-            5.4
-          else
-            5.6
-          end
-        else
-          if member.name == :initialize
-            6.2
-          elsif member.visibility == :public
-            6.4
-          else
-            6.6
-          end
-        end
-      when Members::Alias
-        if member.singleton?
-          5.4
-        else
-          6.4
-        end
-      else
-        raise
-      end
-    end
-
-    def key(member)
-      case member
-      when Members::Include, Members::Extend, Members::Prepend
-        member.name.to_s
-      when Members::ClassVariable, Members::ClassInstanceVariable, Members::InstanceVariable
-        member.name.to_s
-      when Members::AttrAccessor, Members::AttrWriter, Members::AttrReader
-        member.name.to_s
-      when Members::MethodDefinition
-        member.name.to_s
-      when Members::Alias
-        member.new_name.to_s
-      when Declarations::Constant
-        member.name.to_s
-      when Declarations::TypeAlias
-        member.name.to_s
-      when Declarations::Class, Declarations::Module, Declarations::Interface
-        member.name.to_s
-      else
-        raise
-      end
-    end
-
     def sort_decl!(decl)
       case decl
       when Declarations::Class, Declarations::Module, Declarations::Interface
+        partitioned = {
+          type_alias_decls: [],
+          constant_decls: [],
+          class_decls: [],
+          mixin_includes: [],
+          mixin_prepends: [],
+          mixin_extends: [],
+          class_variables: [],
+          class_instance_variables: [],
+          instance_variables: [],
+          singleton_attributes: [],
+          instance_attributes: [],
+          module_functions: [],
+          singleton_new_methods: [],
+          public_singleton_methods: [],
+          private_singleton_methods: [],
+          instance_initialize_methods: [],
+          public_instance_methods: [],
+          private_instance_methods: [],
+        } #: partitioned
+
         decl.members.each { |m| sort_decl! m }
 
-        current_visibility = :public
-        decl.members.map! do |m|
-          case m
+        visibility_annotated_members = [] #: Array[member]
+
+        decl.members.inject(:public) do |current_visibility, member| #$ AST::Members::visibility
+          case member
           when Members::Public
-            current_visibility = :public
-            nil
+            :public
           when Members::Private
-            current_visibility = :private
-            nil
+            :private
           when Members::MethodDefinition, Members::AttrReader, Members::AttrWriter, Members::AttrAccessor
-            m.update(visibility: m.visibility || current_visibility)
+            visibility_annotated_members << member.update(visibility: member.visibility || current_visibility)
+            current_visibility
           else
-            m
-          end
-        end
-        decl.members.compact!
-
-        decl.members.sort! do |m1, m2|
-          group1 = group(m1)
-          group2 = group(m2)
-
-          if group1 == group2
-            key(m1) <=> key(m2)
-          else
-            group1 <=> group2
+            visibility_annotated_members << member
+            current_visibility
           end
         end
 
-        current_visibility = :public
-        decl.members.map! do |m|
-          case m
-          when Members::MethodDefinition, Members::AttrReader, Members::AttrWriter, Members::AttrAccessor
-            new = m.update(visibility: nil)
-            cur = current_visibility
-            current_visibility = m.visibility
-            if cur != m.visibility
-              [
-                m.visibility == :public ? Members::Public.new(location: nil) : Members::Private.new(location: nil),
-                new
-              ]
+        visibility_annotated_members.each do |member|
+          case member
+          when Declarations::TypeAlias
+            partitioned[:type_alias_decls] << member
+          when Declarations::Constant
+            partitioned[:constant_decls] << member
+          when Declarations::Class, Declarations::Module, Declarations::Interface
+            partitioned[:class_decls] << member
+          when Members::Include
+            partitioned[:mixin_includes] << member
+          when Members::Prepend
+            partitioned[:mixin_prepends] << member
+          when Members::Extend
+            partitioned[:mixin_extends] << member
+          when Members::ClassVariable
+            partitioned[:class_variables] << member
+          when Members::ClassInstanceVariable
+            partitioned[:class_instance_variables] << member
+          when Members::InstanceVariable
+            partitioned[:instance_variables] << member
+          when Members::AttrAccessor, Members::AttrWriter, Members::AttrReader
+            if member.kind == :singleton
+              partitioned[:singleton_attributes] << member.update(visibility: nil)
             else
-              new
+              partitioned[:instance_attributes] << member.update(visibility: nil)
             end
+          when Members::MethodDefinition
+            case member.kind
+            when :singleton_instance
+              partitioned[:module_functions] << member.update(visibility: nil)
+            when :singleton
+              if member.name == :new
+                partitioned[:singleton_new_methods] << member.update(visibility: nil)
+              elsif member.visibility == :public
+                partitioned[:public_singleton_methods] << member.update(visibility: nil)
+              else
+                partitioned[:private_singleton_methods] << member.update(visibility: nil)
+              end
+            else
+              if member.name == :initialize
+                partitioned[:instance_initialize_methods] << member.update(visibility: nil)
+              elsif member.visibility == :public
+                partitioned[:public_instance_methods] << member.update(visibility: nil)
+              else
+                partitioned[:private_instance_methods] << member.update(visibility: nil)
+              end
+            end
+          when Members::Alias
+            if member.singleton?
+              partitioned[:public_singleton_methods] << member
+            else
+              partitioned[:public_instance_methods] << member
+            end
+          when Members::Public, Members::Private
+            raise
           else
-            m
+            partitioned[:other_decls] << member
           end
         end
-        decl.members.flatten!
+
+        partitioned[:type_alias_decls].sort_by! {|decl| decl.name.to_s }
+        partitioned[:constant_decls].sort_by! {|decl| decl.name.to_s }
+        partitioned[:class_decls].sort_by! {|decl| decl.name.to_s }
+        partitioned[:mixin_includes].sort_by! {|decl| decl.name.to_s }
+        partitioned[:mixin_prepends].sort_by! {|decl| decl.name.to_s }
+        partitioned[:mixin_extends].sort_by! {|decl| decl.name.to_s }
+        partitioned[:class_variables].sort_by! {|decl| decl.name.to_s }
+        partitioned[:class_instance_variables].sort_by! {|decl| decl.name.to_s }
+        partitioned[:instance_variables].sort_by! {|decl| decl.name.to_s }
+        partitioned[:singleton_attributes].sort_by! {|decl| decl.name.to_s }
+        partitioned[:instance_attributes].sort_by! {|decl| decl.name.to_s }
+        partitioned[:module_functions].sort_by! {|decl| decl.name.to_s }
+        partitioned[:public_singleton_methods].sort_by! {|decl| decl.is_a?(Members::MethodDefinition) ? decl.name.to_s : decl.new_name.to_s }
+        partitioned[:private_singleton_methods].sort_by! {|decl| decl.name.to_s }
+        partitioned[:public_instance_methods].sort_by! {|decl| decl.is_a?(Members::MethodDefinition) ? decl.name.to_s : decl.new_name.to_s }
+        partitioned[:private_instance_methods].sort_by! {|decl| decl.name.to_s }
+
+        members = [] #: Array[member]
+        members.push(*partitioned[:type_alias_decls])
+        members.push(*partitioned[:constant_decls])
+        members.push(*partitioned[:class_decls])
+        members.push(*partitioned[:mixin_includes])
+        members.push(*partitioned[:mixin_prepends])
+        members.push(*partitioned[:mixin_extends])
+        members.push(*partitioned[:class_variables])
+        members.push(*partitioned[:class_instance_variables])
+        members.push(*partitioned[:instance_variables])
+        members.push(*partitioned[:module_functions])
+        members.push(*partitioned[:singleton_attributes])
+
+        current_visibility = :public #: AST::Members::visibility
+
+        members.push(*partitioned[:singleton_new_methods])
+        members.push(*partitioned[:public_singleton_methods])
+
+        if !partitioned[:private_singleton_methods].empty?
+          current_visibility = :private
+          members.push Members::Private.new(location: nil)
+        end
+        members.push(*partitioned[:private_singleton_methods])
+
+        if current_visibility == :private && !partitioned[:public_instance_methods].empty?
+          current_visibility = :public
+          members.push Members::Public.new(location: nil)
+        end
+        members.push(*partitioned[:instance_attributes])
+        members.push(*partitioned[:instance_initialize_methods])
+        members.push(*partitioned[:public_instance_methods])
+
+        if current_visibility == :public && !partitioned[:private_instance_methods].empty?
+          current_visibility = :private
+          members.push Members::Private.new(location: nil)
+        end
+        members.push(*partitioned[:private_instance_methods])
+
+        members.push(*partitioned[:other_decls])
+
+        decl.members.replace(_ = members)
       end
     end
   end
