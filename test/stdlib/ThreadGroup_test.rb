f102d86526ce450ca798985b384303ddaca0be26
SamW <mail@sampersand.me>
2023-09-08 02:27:46 -0500

    Updated ThreadGroup

--- test/stdlib/ThreadGroup_test.rb
+++ test/stdlib/ThreadGroup_test.rb
@@ -1,29 +1,51 @@
 require_relative "test_helper"
 
-class ThreadGroupTest < StdlibTest
-  target ThreadGroup
+class ThreadGroupInstanceTest < Test::Unit::TestCase
+  include TypeAssertions
+
+  testing "::ThreadGroup"
+
+  def test_Default
+    assert_const_type 'ThreadGroup', 'ThreadGroup::Default'
+  end
 
   def test_add
-    tg = ThreadGroup.new
-    tg.add(Thread.new {})
+    thr = Thread.new{}
+    assert_send_type  '(Thread) -> self',
+                      ThreadGroup.new, :add, thr
+  ensure
+    thr.kill
   end
 
   def test_enclose
-    tg = ThreadGroup.new
-    tg.enclose
+    assert_send_type  '() -> self',
+                      ThreadGroup.new, :enclose
   end
 
   def test_enclosed?
     tg = ThreadGroup.new
-    tg.enclosed?
+
+    assert_send_type  '() -> bool',
+                      tg, :enclosed?
+
     tg.enclose
-    tg.enclosed?
+
+    assert_send_type  '() -> bool',
+                      tg, :enclosed?
   end
 
   def test_list
     tg = ThreadGroup.new
-    tg.list
-    tg.add(Thread.new {})
-    tg.list
+    thr = Thread.new{}
+
+    assert_send_type  '() -> Array[Thread]',
+                      tg, :list
+
+    tg.add(thr)
+
+    assert_send_type  '() -> Array[Thread]',
+                      tg, :list
+  ensure
+    thr.kill
   end
 end
