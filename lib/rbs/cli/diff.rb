454a873b88d52c9132a5792adc86adad6af459e2
ksss <co000ri@gmail.com>
2023-11-17 15:15:22 +0900

    Add `--detail` option to rbs diff

--- lib/rbs/cli/diff.rb
+++ lib/rbs/cli/diff.rb
@@ -13,6 +13,7 @@ module RBS
         library_options = library_options
         before_path = []
         after_path = []
+        detail = false
 
         opt = OptionParser.new do |o|
           o.banner = <<~HELP
@@ -35,6 +36,7 @@ module RBS
           o.on("--type-name NAME") { |arg| type_name = arg }
           o.on("--before DIR")     { |arg| before_path << arg }
           o.on("--after DIR")      { |arg| after_path << arg }
+          o.on("--[no-]detail")    { |arg| detail = arg }
         end
         opt.parse!(argv)
 
@@ -47,7 +49,8 @@ module RBS
           type_name: TypeName(type_name).absolute!,
           library_options: library_options,
           after_path: after_path,
-          before_path: before_path
+          before_path: before_path,
+          detail: detail,
         )
       end
 
