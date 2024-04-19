cc5d62571d0f29eaa80c7412d68448eae2924491
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2024-04-19 15:47:18 +0900

    Remove deprecated API since RBS v1

--- lib/rbs/errors.rb
+++ lib/rbs/errors.rb
@@ -62,20 +62,6 @@ module RBS
 
       super "#{Location.to_string location}: Syntax error: #{error_message}, token=`#{location.source}` (#{token_type})"
     end
-
-    def error_value
-      RBS.print_warning {
-        "#{self.class.name}#error_value is deprecated and will be deleted in RBS 2.0. Consider using `location.source` instead."
-      }
-      location.source
-    end
-
-    def token_str
-      RBS.print_warning {
-        "#{self.class.name}#token_str is deprecated and will be deleted in RBS 2.0. Consider using `token_type` instead."
-      }
-      token_type
-    end
   end
 
   class InvalidTypeApplicationError < DefinitionError
--- lib/rbs/parser_compat/lexer_error.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-RBS.print_warning {
-  "RBS::Parser::LexerError is deprecated and will be deleted in RBS 2.0."
-}
-RBS::Parser::LexerError = RBS::ParsingError
