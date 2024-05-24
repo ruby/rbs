c7451e3aa8ad7da347676d897eb8515f113c7ad1
ksss <co000ri@gmail.com>
2024-05-24 22:57:08 +0900

    Ruby API

--- lib/rbs/parser_aux.rb
+++ lib/rbs/parser_aux.rb
@@ -19,6 +19,43 @@ module RBS
       [buf, dirs, decls]
     end
 
+    class LexResult
+      attr_reader :buffer
+      attr_reader :value
+
+      def initialize(buffer:, value:)
+        @buffer = buffer
+        @value = value
+      end
+    end
+
+    class Token
+      attr_reader :type
+      attr_reader :location
+
+      def initialize(type:, location:)
+        @type = type
+        @location = location
+      end
+
+      def value
+        @location.source
+      end
+
+      def comment?
+        @type == :tCOMMENT || @type == :tLINECOMMENT
+      end
+    end
+
+    def self.lex(source)
+      buf = buffer(source)
+      value = _lex(buf, buf.last_position)
+      value.map! do |type, location|
+        Token.new(type: type, location: location)
+      end
+      LexResult.new(buffer: buf, value: value)
+    end
+
     def self.buffer(source)
       case source
       when String
