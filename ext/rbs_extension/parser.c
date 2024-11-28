1ab4c329961d8ea825563c4f6396842f453336fb
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-11-28 18:31:37 -0500

    Make `parse_type_params` return a NODISCARD bool
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- ext/rbs_extension/parser.c
+++ ext/rbs_extension/parser.c
@@ -1258,8 +1258,9 @@ bool parse_type(parserstate *state, rbs_node_t **type) {
 
   type_param ::= tUIDENT upper_bound? default_type?                           (module_type_params == false)
 */
-static rbs_node_list_t *parse_type_params(parserstate *state, range *rg, bool module_type_params) {
-  rbs_node_list_t *params = rbs_node_list_new(&state->allocator);
+NODISCARD
+static bool parse_type_params(parserstate *state, range *rg, bool module_type_params, rbs_node_list_t **params) {
+  *params = rbs_node_list_new(&state->allocator);
 
   bool required_param_allowed = true;
 
@@ -1296,7 +1297,7 @@ static rbs_node_list_t *parse_type_params(parserstate *state, range *rg, bool mo
             break;
           default:
             state->aborted = true;
-            return NULL;
+            return false;
           }
 
           parser_advance(state);
@@ -1338,7 +1339,7 @@ static rbs_node_list_t *parse_type_params(parserstate *state, range *rg, bool mo
         } else {
           if (!required_param_allowed) {
             syntax_error(state, state->current_token, "required type parameter is not allowed after optional type parameter");
-            return NULL;
+            return false;
           }
         }
       }
@@ -1355,7 +1356,7 @@ static rbs_node_list_t *parse_type_params(parserstate *state, range *rg, bool mo
 
       rbs_ast_typeparam_t *param = rbs_ast_typeparam_new(&state->allocator, name, variance, upper_bound, default_type, unchecked, loc);
 
-      rbs_node_list_append(params, (rbs_node_t *) param);
+      rbs_node_list_append(*params, (rbs_node_t *) param);
 
       if (state->next_token.type == pCOMMA) {
         parser_advance(state);
@@ -1372,7 +1373,7 @@ static rbs_node_list_t *parse_type_params(parserstate *state, range *rg, bool mo
     *rg = NULL_RANGE;
   }
 
-  return params;
+  return true;
 }
 
 /*
@@ -1386,8 +1387,8 @@ bool parse_method_type(parserstate *state, rbs_methodtype_t **method_type) {
   rg.start = state->next_token.range.start;
 
   range params_range = NULL_RANGE;
-  rbs_node_list_t *type_params = parse_type_params(state, &params_range, false); // TODO
-  if (has_error(state)) return false;
+  rbs_node_list_t *type_params;
+  CHECK_PARSE(parse_type_params(state, &params_range, false, &type_params));
 
   range type_range;
   type_range.start = state->next_token.range.start;
@@ -1489,8 +1490,8 @@ static bool parse_type_decl(parserstate *state, position comment_pos, rbs_node_l
   CHECK_PARSE(parse_type_name(state, ALIAS_NAME, &name_range, &typename));
 
   range params_range;
-  rbs_node_list_t *type_params = parse_type_params(state, &params_range, true);
-  if (has_error(state)) return false;
+  rbs_node_list_t *type_params;
+  CHECK_PARSE(parse_type_params(state, &params_range, true, &type_params));
 
   parser_advance_assert(state, pEQ);
   range eq_range = state->current_token.range;
@@ -2353,8 +2354,8 @@ static bool parse_interface_decl(parserstate *state, position comment_pos, rbs_n
   CHECK_PARSE(parse_type_name(state, INTERFACE_NAME, &name_range, &name));
 
   range type_params_range;
-  rbs_node_list_t *type_params = parse_type_params(state, &type_params_range, true);
-  if (has_error(state)) return false;
+  rbs_node_list_t *type_params;
+  CHECK_PARSE(parse_type_params(state, &type_params_range, true, &type_params));
 
   rbs_node_list_t *members = NULL;
   CHECK_PARSE(parse_interface_members(state, &members));
@@ -2535,8 +2536,8 @@ static bool parse_module_decl0(parserstate *state, range keyword_range, rbs_type
   decl_range.start = keyword_range.start;
 
   range type_params_range;
-  rbs_node_list_t *type_params = parse_type_params(state, &type_params_range, true);
-  if (has_error(state)) return false;
+  rbs_node_list_t *type_params;
+  CHECK_PARSE(parse_type_params(state, &type_params_range, true, &type_params));
 
   rbs_node_list_t *self_types = rbs_node_list_new(&state->allocator);
   range colon_range;
@@ -2667,8 +2668,8 @@ static bool parse_class_decl0(parserstate *state, range keyword_range, rbs_typen
   decl_range.start = keyword_range.start;
 
   range type_params_range;
-  rbs_node_list_t *type_params = parse_type_params(state, &type_params_range, true);
-  if (has_error(state)) return false;
+  rbs_node_list_t *type_params;
+  CHECK_PARSE(parse_type_params(state, &type_params_range, true, &type_params));
 
   range lt_range;
   rbs_ast_declarations_class_super_t *super = NULL;
