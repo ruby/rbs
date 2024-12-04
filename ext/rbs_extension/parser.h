9a28ef6cc900022108c0dd21c039b44253288091
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-12-03 19:15:32 -0500

    Replace Ruby’s `PRINTF_ARGS`

--- ext/rbs_extension/parser.h
+++ ext/rbs_extension/parser.h
@@ -1,9 +1,10 @@
 #ifndef RBS__PARSER_H
 #define RBS__PARSER_H
 
+#include "rbs/defines.h"
 #include "parserstate.h"
 
-PRINTF_ARGS(void set_error(parserstate *state, token tok, bool syntax_error, const char *fmt, ...), 4, 5);
+void set_error(parserstate *state, token tok, bool syntax_error, const char *fmt, ...) RBS_ATTRIBUTE_FORMAT(4, 5);
 
 bool parse_type(parserstate *state, rbs_node_t **type);
 bool parse_method_type(parserstate *state, rbs_methodtype_t **method_type);
---
+++ include/rbs/defines.h
@@ -0,0 +1,24 @@
+// This file contains a modified subset of Prism's `include/prism/defines.h`
+
+#ifndef RBS_DEFINES_H
+#define RBS_DEFINES_H
+
+/**
+ * Certain compilers support specifying that a function accepts variadic
+ * parameters that look like printf format strings to provide a better developer
+ * experience when someone is using the function. This macro does that in a
+ * compiler-agnostic way.
+ */
+#if defined(__GNUC__)
+#   if defined(__MINGW_PRINTF_FORMAT)
+#       define RBS_ATTRIBUTE_FORMAT(string_index, argument_index) __attribute__((format(__MINGW_PRINTF_FORMAT, string_index, argument_index)))
+#   else
+#       define RBS_ATTRIBUTE_FORMAT(string_index, argument_index) __attribute__((format(printf, string_index, argument_index)))
+#   endif
+#elif defined(__clang__)
+#   define RBS_ATTRIBUTE_FORMAT(string_index, argument_index) __attribute__((__format__(__printf__, string_index, argument_index)))
+#else
+#   define RBS_ATTRIBUTE_FORMAT(string_index, argument_index)
+#endif
+
+#endif
