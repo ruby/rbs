e5cc95a99fbb09236b2479a1a7ffe246258ebf1d
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-12-17 12:19:50 -0500

    Copy RBS Strings into Ruby
    Instead of using `rb_str_new_static` to reference immortal RBS strings.

--- ext/rbs_extension/rbs_string_bridging.c
+++ ext/rbs_extension/rbs_string_bridging.c
@@ -5,7 +5,5 @@ rbs_string_t rbs_string_from_ruby_string(VALUE ruby_string) {
 }
 
 VALUE rbs_string_to_ruby_string(rbs_string_t *self, rb_encoding *encoding) {
-    VALUE str = rb_str_new_static(self->start, rbs_string_len(*self));
-    rb_enc_associate(str, encoding);
-    return str;
+    return rb_enc_str_new(self->start, rbs_string_len(*self), encoding);
 }
