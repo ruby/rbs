43c418e42a82427ee73480ad18ec82e54e66adac
ksss <co000ri@gmail.com>
2025-03-18 17:35:37 +0900

    Implement context validation on parse

--- ext/rbs_extension/parser.h
+++ ext/rbs_extension/parser.h
@@ -9,8 +9,16 @@
  * */
 extern VALUE RBS_Parser;
 
-VALUE parse_type(parserstate *state);
-VALUE parse_method_type(parserstate *state);
+typedef enum {
+  vcNull = 0,
+  vcNoVoid = 1,
+  vcNoVoidAllowedHere = 2,
+  vcNoSelf = 4,
+  vcNoClassish = 8,
+} ValidationContext;
+
+VALUE parse_type(parserstate *state, ValidationContext vc);
+VALUE parse_method_type(parserstate *state, ValidationContext vc);
 VALUE parse_signature(parserstate *state);
 
 void rbs__init_parser();
