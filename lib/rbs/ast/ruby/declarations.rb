03a9adb1c054569f2e5a9098f5609a63336421b1
Soutaro Matsumoto <matsumoto@soutaro.com>
2025-04-03 16:39:56 +0900

    Add `AST::Ruby::Declarations` module and class/module declarations

---
+++ lib/rbs/ast/ruby/declarations.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+module RBS
+  module AST
+    module Ruby
+      module Declarations
+        class Base
+          attr_reader :buffer
+
+          def initialize(buffer)
+            @buffer = buffer
+          end
+
+          def constant_as_type_name(node)
+            case node
+            when Prism::ConstantPathNode, Prism::ConstantReadNode
+              TypeName.parse(node.full_name)
+            end
+          end
+
+          def rbs_location(location)
+            Location.new(buffer, location.start_character_offset, location.end_character_offset)
+          end
+        end
+
+        class ClassDecl < Base
+          attr_reader :class_name
+
+          attr_reader :members
+
+          def initialize(buffer, name, node)
+            super(buffer)
+            @class_name = name
+            @node = node
+            @members = []
+          end
+
+          def each_decl(&block)
+            return enum_for(:each_decl) unless block
+
+            @members.each do |member|
+              if member.is_a?(Base)
+                yield member
+              end
+            end
+          end
+        end
+
+        class ModuleDecl < Base
+          attr_reader :module_name
+
+          attr_reader :members
+
+          def initialize(buffer, name, node)
+            super(buffer)
+            @module_name = name
+            @node = node
+            @members = []
+          end
+
+          def each_decl(&block)
+            return enum_for(:each_decl) unless block
+
+            @members.each do |member|
+              if member.is_a?(Base)
+                yield member
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
