575fc744b42bdefa15fb2e5dae23ec80c2a0b4dc
Stan Lo <stan.lo@shopify.com>
2025-05-20 21:37:13 +0100

    Fix alignof compatibility issue

--- include/rbs/util/rbs_allocator.h
+++ include/rbs/util/rbs_allocator.h
@@ -3,15 +3,32 @@
 
 #include <stddef.h>
 
-#ifndef alignof
-#if defined(__GNUC__) || defined(__clang__)
-#define alignof(type) __alignof__(type)
-#elif defined(_MSC_VER)
-#define alignof(type) __alignof(type)
-#else
-// Fallback using offset trick
-#define alignof(type) offsetof(struct { char c; type member; }, member)
+/* Include stdalign.h for C11 and later for alignof support */
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+#include <stdalign.h>
 #endif
+
+/*
+ * Define a portable alignment macro that works across all supported environments
+ */
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+/* C11 or later - use _Alignof directly (always available in C11+) */
+#define rbs_alignof(type) _Alignof(type)
+#elif defined(__cplusplus) && __cplusplus >= 201103L
+/* C++11 or later has alignof keyword */
+#define rbs_alignof(type) alignof(type)
+#elif defined(__GNUC__) || defined(__clang__)
+/* GCC and Clang provide __alignof__ */
+#define rbs_alignof(type) __alignof__(type)
+#elif defined(_MSC_VER)
+/* MSVC provides __alignof */
+#define rbs_alignof(type) __alignof(type)
+#else
+/* Fallback using offset trick for other compilers */
+#define rbs_alignof(type) offsetof( \
+    struct { char c; type member; },                     \
+    member                          \
+)
 #endif
 
 struct rbs_allocator;
@@ -26,13 +43,13 @@ void *rbs_allocator_calloc_impl(rbs_allocator_t *, size_t count, size_t size, si
 void *rbs_allocator_realloc_impl(rbs_allocator_t *, void *ptr, size_t old_size, size_t new_size, size_t alignment);
 
 // Use this when allocating memory for a single instance of a type.
-#define rbs_allocator_alloc(allocator, type) ((type *) rbs_allocator_malloc_impl((allocator), sizeof(type), alignof(type)))
+#define rbs_allocator_alloc(allocator, type) ((type *) rbs_allocator_malloc_impl((allocator), sizeof(type), rbs_alignof(type)))
 // Use this when allocating memory that will be immediately written to in full.
 // Such as allocating strings
-#define rbs_allocator_alloc_many(allocator, count, type) ((type *) rbs_allocator_malloc_many_impl((allocator), (count), sizeof(type), alignof(type)))
+#define rbs_allocator_alloc_many(allocator, count, type) ((type *) rbs_allocator_malloc_many_impl((allocator), (count), sizeof(type), rbs_alignof(type)))
 // Use this when allocating memory that will NOT be immediately written to in full.
 // Such as allocating buffers
-#define rbs_allocator_calloc(allocator, count, type) ((type *) rbs_allocator_calloc_impl((allocator), (count), sizeof(type), alignof(type)))
-#define rbs_allocator_realloc(allocator, ptr, old_size, new_size, type) ((type *) rbs_allocator_realloc_impl((allocator), (ptr), (old_size), (new_size), alignof(type)))
+#define rbs_allocator_calloc(allocator, count, type) ((type *) rbs_allocator_calloc_impl((allocator), (count), sizeof(type), rbs_alignof(type)))
+#define rbs_allocator_realloc(allocator, ptr, old_size, new_size, type) ((type *) rbs_allocator_realloc_impl((allocator), (ptr), (old_size), (new_size), rbs_alignof(type)))
 
 #endif
