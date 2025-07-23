ab3c04792568faad4e546c2d6c9c1192a45a468e
ksss <co000ri@gmail.com>
2025-07-23 10:08:28 +0900

    Remove undocumented method `#zero?`

--- test/stdlib/Numeric_test.rb
+++ test/stdlib/Numeric_test.rb
@@ -16,4 +16,9 @@ class NumericInstanceTest < Test::Unit::TestCase
     assert_send_type '() -> true', 1.0, :real?
     assert_send_type '() -> true', 1r, :real?
   end
+
+  def test_zero?
+    assert_send_type '() -> bool', 0, :zero?
+    assert_send_type '() -> bool', 1r, :zero?
+  end
 end
