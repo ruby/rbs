9aa9051378deac9ae9364784f27f1aab9b2e846d
ksss <co000ri@gmail.com>
2025-11-05 23:47:39 +0900

    Add type of `Open3.popen3`

--- test/stdlib/Open3_test.rb
+++ test/stdlib/Open3_test.rb
@@ -27,6 +27,24 @@ class Open3SingletonTest < Test::Unit::TestCase
     assert_send_type "(::Hash[::String, ::String], *::String) -> [ ::String, ::Process::Status ]",
                      Open3, :capture2e, { 'FOO' => 'BAR' }, "echo $FOO"
   end
+
+  def test_popen3
+    assert_send_type "(::String) -> [ ::IO, ::IO, ::IO, ::Process::Waiter ]",
+                     Open3, :popen3, 'echo "Foo"'
+    assert_send_type "(::String, unsetenv_others: bool) -> [ ::IO, ::IO, ::IO, ::Process::Waiter ]",
+                     Open3, :popen3, 'env', unsetenv_others: true
+    assert_send_type "(::String, close_others: bool) -> [ ::IO, ::IO, ::IO, ::Process::Waiter ]",
+                     Open3, :popen3, 'env', close_others: true
+    assert_send_type "(::String, chdir: ::String) -> [ ::IO, ::IO, ::IO, ::Process::Waiter ]",
+                     Open3, :popen3, 'echo "Foo"', chdir: '.'
+    assert_send_type "(::Hash[::String, ::String], ::String) -> [ ::IO, ::IO, ::IO, ::Process::Waiter ]",
+                     Open3, :popen3, { 'FOO' => 'BAR' }, "echo $FOO"
+
+    assert_send_type "(::String) { (::IO, ::IO, ::IO, ::Process::Waiter) -> [::IO, ::IO, ::IO, ::Process::Waiter] } -> [::IO, ::IO, ::IO, ::Process::Waiter]",
+                     Open3, :popen3, 'echo "Foo"' do |*args| args end
+    assert_send_type "(::Hash[::String, ::String], ::String) { (::IO, ::IO, ::IO, ::Process::Waiter) -> [::IO, ::IO, ::IO, ::Process::Waiter] } -> [::IO, ::IO, ::IO, ::Process::Waiter]",
+                     Open3, :popen3, { 'FOO' => 'BAR' }, 'echo $FOO' do |*args| args end
+  end
 end
 
 class Open3InstanceTest < Test::Unit::TestCase
@@ -47,5 +65,10 @@ class Open3InstanceTest < Test::Unit::TestCase
   def test_capture2e
     assert_send_type "(*::String) -> [ ::String, ::Process::Status ]",
                      CustomOpen3.new, :capture2e, 'echo "Foo"'
+  end
+
+  def test_popen3
+    assert_send_type "(::String) -> [ ::IO, ::IO, ::IO, ::Process::Waiter ]",
+                     CustomOpen3.new, :popen3, 'echo "Foo"'
   end
 end
