ae940d490411257b18ae59b8182a27da0e46e2f5
ksss <co000ri@gmail.com>
2025-12-19 13:59:52 +0900

    Add test case for tuple

--- test/rbs/test/type_check_test.rb
+++ test/rbs/test/type_check_test.rb
@@ -67,6 +67,10 @@ EOF
         refute typecheck.value(nil, parse_type("bool"))
         refute typecheck.value("", parse_type("bool"))
         refute typecheck.value(BasicObject.new, parse_type("bool"))
+
+        assert typecheck.value([1], parse_type("[Integer]"))
+        refute typecheck.value([], parse_type("[Integer]"))
+        refute typecheck.value([1, 2], parse_type("[Integer]"))
       end
     end
   end
