4698564950363f70358f60de81ff8c1f9509345c
HoneyryderChuck <cardoso_tiago@hotmail.com>
2026-01-14 12:29:16 +0000

    pstore: fixes types, treat it as a collection
    this adds hash-like typing to signal its key-value store semantics. it
    seems like a breaking change, but in practice this has been broken for a
    while.
    
    Other wrong sigs were fixed, such as transaction, which requires a
    block.

--- test/stdlib/PStore_test.rb
+++ test/stdlib/PStore_test.rb
@@ -7,9 +7,32 @@ class PStoreSingletonTest < Test::Unit::TestCase
   testing "singleton(::PStore)"
 
   def test_initialize
-    assert_send_type  "(untyped file, ?bool thread_safe) -> PStore",
+    assert_send_type  "(path file, ?bool thread_safe) -> PStore",
                       PStore, :new, "file_name", false
-    assert_send_type  "(untyped file, ?Symbol) -> PStore",
+    assert_send_type  "(path file, ?Symbol) -> PStore",
                       PStore, :new, "file_name", :false
+    assert_send_type  "(path file, ?bool thread_safe) -> PStore[Integer]",
+                      PStore, :new, "file_name", false
+    assert_send_type  "(path file, ?Symbol) -> PStore[Integer, Integer]",
+                      PStore, :new, "file_name", :false
+  end
+
+end
+
+class PStoreInstanceTest < Test::Unit::TestCase
+  include TestHelper
+  library "pstore"
+  testing "::PStore[Symbol, Integer]"
+
+  def test_accessors
+    store = PStore.new("file_name", false)
+    store.transaction do |st|
+      st[:foo] = 1
+
+      assert_send_type  "(Symbol) -> Integer",
+                        st, :[], :foo
+      assert_send_type  "(Symbol) -> nil",
+                        st, :[], :bar
+    end
   end
 end
