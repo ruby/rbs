951e57dc21ae17c9e22b16f193f91a7199ed9c10
ksss <co000ri@gmail.com>
2026-01-21 11:59:38 +0900

    Try asan

--- ext/rbs_extension/main.c
+++ ext/rbs_extension/main.c
@@ -466,6 +466,10 @@ void Init_rbs_extension(void) {
 #ifdef HAVE_RB_EXT_RACTOR_SAFE
     rb_ext_ractor_safe(true);
 #endif
+    // ASAN test: intentional heap buffer overflow to verify ASAN is working
+    volatile char *test_ptr = malloc(10);
+    test_ptr[10] = 'A'; // Out of bounds write - this will be caught by ASAN
+
     rbs__init_constants();
     rbs__init_location();
     rbs__init_parser();
