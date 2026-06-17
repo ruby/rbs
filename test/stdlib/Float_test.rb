c1a53542311c2a7083956ab1ee6cb16ece4f756c
SamW <mail@sampersand.me>
2026-06-17 09:57:48 -0700

    added instance method shell

--- test/stdlib/Float_test.rb
+++ test/stdlib/Float_test.rb
@@ -66,7 +66,7 @@ class FloatSingletonTest < Test::Unit::TestCase
   end
 
   def test_constant_ROUNDS
-    notify "Skipping test: Float::ROUNDS does not exist" unless defined? Float::ROUNDS
+    return unless defined? Float::ROUNDS
     assert_const_type 'Integer',
                       'Float::ROUNDS'
   end
@@ -76,4 +76,192 @@ class FloatInstanceTest < Test::Unit::TestCase
   include TestHelper
 
   testing 'Float'
+
+  def test_op_mod
+    omit 'todo'
+  end
+
+  def test_op_mul
+    omit 'todo'
+  end
+
+  def test_op_pow
+    omit 'todo'
+  end
+
+  def test_op_add
+    omit 'todo'
+  end
+
+  def test_op_sub
+    omit 'todo'
+  end
+
+  def test_op_uneg
+    omit 'todo'
+  end
+
+  def test_op_div
+    omit 'todo'
+  end
+
+  def test_op_lth
+    omit 'todo'
+  end
+
+  def test_op_leq
+    omit 'todo'
+  end
+
+  def test_op_cmp
+    omit 'todo'
+  end
+
+  def test_op_eq
+    omit 'todo'
+  end
+
+  def test_op_eqq
+    omit 'todo'
+  end
+
+  def test_op_gth
+    omit 'todo'
+  end
+
+  def test_op_geq
+    omit 'todo'
+  end
+
+  def test_abs
+    omit 'todo'
+  end
+
+  def test_angle
+    omit 'todo'
+  end
+
+  def test_arg
+    omit 'todo'
+  end
+
+  def test_ceil
+    omit 'todo'
+  end
+
+  def test_coerce
+    omit 'todo'
+  end
+
+  def test_denominator
+    omit 'todo'
+  end
+
+  def test_divmod
+    omit 'todo'
+  end
+
+  def test_eql?
+    omit 'todo'
+  end
+
+  def test_fdiv
+    omit 'todo'
+  end
+
+  def test_finite?
+    omit 'todo'
+  end
+
+  def test_floor
+    omit 'todo'
+  end
+
+  def test_hash
+    omit 'todo'
+  end
+
+  def test_infinite?
+    omit 'todo'
+  end
+
+  def test_inspect
+    omit 'todo'
+  end
+
+  def test_magnitude
+    omit 'todo'
+  end
+
+  def test_modulo
+    omit 'todo'
+  end
+
+  def test_nan?
+    omit 'todo'
+  end
+
+  def test_negative?
+    omit 'todo'
+  end
+
+  def test_next_float
+    omit 'todo'
+  end
+
+  def test_numerator
+    omit 'todo'
+  end
+
+  def test_phase
+    omit 'todo'
+  end
+
+  def test_positive?
+    omit 'todo'
+  end
+
+  def test_prev_float
+    omit 'todo'
+  end
+
+  def test_quo
+    omit 'todo'
+  end
+
+  def test_rationalize
+    omit 'todo'
+  end
+
+  def test_round
+    omit 'todo'
+  end
+
+  def test_to_f
+    omit 'todo'
+  end
+
+  def test_to_i
+    omit 'todo'
+  end
+
+  def test_to_int
+    omit 'todo'
+  end
+
+  def test_to_r
+    omit 'todo'
+  end
+
+  def test_to_s
+    omit 'todo'
+  end
+
+  def test_truncate
+    omit 'todo'
+  end
+
+  def test_zero?
+    omit 'todo'
+  end
 end
