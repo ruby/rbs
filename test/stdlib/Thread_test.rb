065ef4c684d505e6866bd18d0594765f03f0ebfd
ksss <co000ri@gmail.com>
2026-09-24 22:12:49 +0900

    Return bool on `Thread#{abort,report}_on_exception`

--- test/stdlib/Thread_test.rb
+++ test/stdlib/Thread_test.rb
@@ -5,6 +5,14 @@ class ThreadSingletonTest < Test::Unit::TestCase
 
   testing "singleton(::Thread)"
 
+  def test_abort_on_exception
+    assert_send_type "() -> bool", Thread, :abort_on_exception
+  end
+
+  def test_report_on_exception
+    assert_send_type "() -> bool", Thread, :report_on_exception
+  end
+
   def test_new
     assert_send_type  "() { () -> untyped } -> Thread",
                       Thread, :new do 1 end
